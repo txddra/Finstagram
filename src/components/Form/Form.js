208151e0a5b4ac8f4e95e202b25f4eb2796cc0b2
LaToddra Belton <latoddra.belton@codeimmersives.com>
2021-05-02 23:59:12 -0500

    created text fields for the title,messages,and tags, similar to creator

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -19,13 +19,25 @@ const handleSubmit =()=>{
         <Paper className={classes.paper}> 
             <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
                 <Typography variant = "h6">Creating a memory</Typography>
-                <TextField
-                name="creator"
-                variant= "outlined"
-                label="Creator"
-                fullWidth value={postData.creator}
-                // keeps data persistent  g
+                {/* for creator */}
+                <TextField name="creator" variant= "outlined" label="Creator" fullWidth value={postData.creator}
+                // keeps data persistent 
                 onChange={(e)=>setPostData({...postData, creator: e.target.value})}
+                ></TextField>
+                {/* for title */}
+                <TextField name="title" variant= "outlined" label="Title" fullWidth value={postData.title}
+                // keeps data persistent 
+                onChange={(e)=>setPostData({...postData, title: e.target.value})}
+                ></TextField>
+                {/* for message */}
+                <TextField name="message" variant= "outlined" label="Message" fullWidth value={postData.message}
+                // keeps data persistent 
+                onChange={(e)=>setPostData({...postData, message: e.target.value})}
+                ></TextField>
+                {/*for tags */}
+                <TextField name="tags" variant= "outlined" label="Tags" fullWidth value={postData.tags}
+                // keeps data persistent 
+                onChange={(e)=>setPostData({...postData, tags: e.target.value})}
                 ></TextField>
 
             </form>
